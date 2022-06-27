933b33b5349a4ce11a063a82b8037fcb13d6af47
PedroParraPerez <pedroparra06091994@gmail.com>
2022-06-27 17:04:42 +0000

    onyle need to create relationship and endpoints for that

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,95 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# .....................take all user, character or planet..................
+@app.route('/users', methods=['GET'])
+def get_all_user():
+    users = User.query.all()
+    user_list =  list(map(lambda user: user.serialize(), users))
+    return ({"results":user_list}), 200
 
-    return jsonify(response_body), 200
+@app.route('/characters', methods=['GET'])
+def get_all_characters():
+    characters = Character.query.all()
+    character_list =  list(map(lambda character: character.serialize(), characters))
+    return ({"results":character_list}), 200
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    planets = Planet.query.all()
+    planet_list =  list(map(lambda planet: planet.serialize(), planets))
+    return ({"results":planet_list}), 200
+
+@app.route('/user/<int:id>', methods=['GET'])
+def get_one_user(id):
+    user = User.query.get(id)
+    return {"results":user.serialize()}, 200
+
+@app.route('/planet/<int:id>', methods=['GET'])
+def get_one_planet(id):
+    user = Planet.query.get(id)
+    return {"results":planet.serialize()}, 200
+
+@app.route('/character/<int:id>', methods=['GET'])
+def get_one_character(id):
+    character = Character.query.get(id)
+    return {"results":character.serialize()}, 200
+
+# .....................................Delete one user, characters or planet.............................
+
+@app.route('/character/<int:id>', methods=['DELETE'])
+def delete_one_character(id):
+
+    character = Character.query.get(id)
+    db.session.delete(character)
+    db.session.commit()
+    return {"delete":character.serialize()}, 200
+
+@app.route('/planet/<int:id>', methods=['DELETE'])
+def delete_one_planet(id):
+
+    planet = Planet.query.get(id)
+    db.session.delete(planet)
+    db.session.commit()
+    return {"delete":planet.serialize()}, 200
+
+@app.route('/user/<int:id>', methods=['DELETE'])
+def delete_one_user(id):
+
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return {"delete":user.serialize()}, 200
+
+# ...............................Create one character, user or planets....................
+
+@app.route('/user', methods=['POST'])
+def create_one_user():
+
+    user = request.get_json()
+    new_user = User(email=user["email"], password=user["password"], is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"created":new_user.serialize()}), 200
+
+@app.route('/planet', methods=['POST'])
+def create_one_planet():
+
+    planet = request.get_json()
+    new_planet = Planet(name=planet["name"], diameter=planet["diameter"], mass=planet["mass"],  orbit=planet["orbit"])
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify({"created":new_planet.serialize()}), 200
+
+@app.route('/character', methods=['POST'])
+def create_one_character():
+
+    character = request.get_json()
+    new_character = Character(name=character["name"], heigth=character["heigth"], weigth=character["weigth"],  eyes_color=character["eyes_color"])
+    db.session.add(new_character)
+    db.session.commit()
+    return jsonify({"created":new_character.serialize()}), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -17,3 +17,44 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    heigth = db.Column(db.Integer, unique=False, nullable=False)
+    weigth = db.Column(db.String(120), unique=False, nullable=False)
+    eyes_color = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "heigth": self.heigth,
+            "weigth": self.weigth,
+            "eyes_color": self.eyes_color,
+            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    diameter = db.Column(db.String(120), unique=False, nullable=False)
+    mass = db.Column(db.String(120), unique=False, nullable=False)
+    orbit = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "mass": self.mass,
+            "orbit": self.orbit,
+            # do not serialize the password, its a security breach
+        }
+
